b165c6a5505e6e8ce57fada1b23e3c3587fe5e7e
Tiago Santos <tisgo.slip@hotmail.com>
2021-04-12 20:19:07 -0300

    wip POO.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+class Produto {
+
+  constructor() {
+    this.id = 0
+    this.nomeProduto = ''
+    this.valor = 0
+  }
+
+  somar() {
+    alert('somar')
+  }
+
+  subtrair() {
+    alert('subtrair')
+  }
+
+  editar() {
+    alert('editar')
+  }
+
+  deletar() {
+    alert('deletar')
+  }
+
+}
+
+var produto = new Produto()
